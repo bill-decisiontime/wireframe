586ecb7eff235308f68a5cc2efa04d0e97add538
Bill Morrison <bill.morrison@teamsolutionz.com>
2018-11-28 21:59:39 +0000

    add fontawesome

--- routes/sqs_route.js
+++ routes/sqs_route.js
@@ -5,14 +5,12 @@ var debug = require('debug')(app_name+':sqs_route');
 
 //aws
 const Consumer = require('sqs-consumer');
-SQS_QUEUE_URL = 'https://sqs.eu-west-2.amazonaws.com/065479731380/billtest';
 
-/* GET home page. */
 router.get('/', function(req, res, next)
 {
   const app = Consumer.create({
     region: 'eu-west-2',
-    queueUrl: SQS_QUEUE_URL,
+    queueUrl: process.env.SQS_QUEUE_URL,
     waitTimeSeconds: 20,
     handleMessage: (message, done) => {
       console.log(message);
